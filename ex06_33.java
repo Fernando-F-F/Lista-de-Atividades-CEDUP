72b99b8651aca48fece76eb3c08dbf0308b0cb2e
Fernando F Frasseto <107114937+Fernando-F-F@users.noreply.github.com>
2022-07-08 17:29:59 -0300

    Update ex06_33.java

--- ex06_33.java
+++ ex06_33.java
@@ -5,20 +5,14 @@ public class ex06_33 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float quilos,libras,quilosCon,librasCon;
-
         System.out.println("Digite o valor em quilos: ");
         quilos = input.nextFloat();
             quilosCon = quilos * 2.205f;
         System.out.println("O valor de quilos para libras é: "+ quilosCon);
-
         System.out.println("Digite o valor em libras: ");
         libras = input.nextFloat();
             librasCon = libras / 2.205f;
         System.out.println("O valor de libras para quilos é: "+ librasCon);
-
-        
-
-
 
     }
 }

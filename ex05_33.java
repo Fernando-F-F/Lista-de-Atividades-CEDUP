3bda5b2a0e1ce38704754582e861cd1344db12bd
Fernando F Frasseto <107114937+Fernando-F-F@users.noreply.github.com>
2022-07-08 17:17:16 -0300

    Add files via upload

---
+++ ex05_33.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex05_33 {
+
+    public static void main(String[] args) {
+        Scanner pagamento = new Scanner(System.in);
+        double horas, salario;
+
+        System.out.println("Informe a quantidade de horas que você trabalhou: ");
+        horas = pagamento.nextDouble();
+
+        salario = (horas * 10.25);
+
+        System.out.println("O seu salário será de : " + salario + " reais");
+
+        if (salario < 50.00)
+            System.out.println("Atenção, dirija-se à direção do Hotel!");
+
+    }
+}
